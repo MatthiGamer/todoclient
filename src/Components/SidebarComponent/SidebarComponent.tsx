5028f7b8773ba7226537ccc7105cebc4e0f6f656
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2023-12-18 15:48:00 +0100

    Updated SidebarComponent

--- src/Components/SidebarComponent/SidebarComponent.tsx
+++ src/Components/SidebarComponent/SidebarComponent.tsx
@@ -5,9 +5,8 @@ import TitleComponent from '../TitleComponent/TitleComponent';
 const SidebarComponent: React.FC = () => {
   return (
     <div id="sidebar">
+      <TitleComponent title='TO DO'/>
       {/* Sidebar content goes here */}
-      <TitleComponent title='Todo List'/>
-      <h1>Todo</h1>
     </div>
   );
 };
