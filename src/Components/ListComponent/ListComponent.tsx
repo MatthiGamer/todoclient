c312db3e184a9dbcf099ec90103db00f220ea553
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2023-12-18 16:12:00 +0100

    Added color constants

---
+++ src/Components/ListComponent/ListComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ListComponent.css";
+
+const ListComponent: React.FC = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default ListComponent;
