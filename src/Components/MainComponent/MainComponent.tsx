d9fe423f833446efc5b1c1164d725ade4336e6c4
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-01-02 20:25:00 +0100

    Added MainContainerComponent and an empty version if no list is selected

--- src/Components/MainComponent/MainComponent.tsx
+++ src/Components/MainComponent/MainComponent.tsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
 import SidebarComponent from '../SidebarComponent/SidebarComponent';
 import './MainComponent.css';
-import TitleComponent from '../TitleComponent/TitleComponent';
-import DividerComponent from '../DividerComponent/DividerComponent';
-import { PRIMARY_COLOR, SECONDARY_COLOR } from '../../Colors';
-import ListItemComponent from '../ListItemComponent/ListItemComponent';
+import MainContainerComponent from '../MainContainerComponent/MainContainerComponent';
 
 const MainComponent: React.FC = () => {
 
@@ -14,9 +11,7 @@ const MainComponent: React.FC = () => {
     <div className="main-container">
       <SidebarComponent setListName={setListName}/>
       <div className="main-content">
-        {/* Main content goes here */}
-        <TitleComponent title={listName} color={SECONDARY_COLOR}/>
-        <DividerComponent color={listName == "" ? PRIMARY_COLOR : SECONDARY_COLOR}/>
+        <MainContainerComponent listName={listName}/>
       </div>
     </div>
   );
---
+++ src/Components/MainContainerComponent/MainContainerComponent.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import TitleComponent from "../TitleComponent/TitleComponent";
+import DividerComponent from "../DividerComponent/DividerComponent";
+import { SECONDARY_COLOR } from "../../Colors";
+import EmptyMainContainerComponent from "../EmptyMainContainerComponent/EmptyMainContainerComponent";
+
+interface MainContainerComponentProps{
+    listName: string;
+}
+
+const MainContainerComponent: React.FC<MainContainerComponentProps> = (props) => {
+    return (
+        props.listName === "" ? <EmptyMainContainerComponent/> : 
+        <>
+            <TitleComponent title={props.listName} color={SECONDARY_COLOR}/>
+            <DividerComponent color={SECONDARY_COLOR}/>
+        </>
+    )
+};
+
+export default MainContainerComponent;
