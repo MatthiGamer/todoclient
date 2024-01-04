b6682615c24c958beab432fe739309ae524cf58d
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-01-04 09:55:00 +0100

    Added CustomEmptyComponent

---
+++ src/Components/EmptyCustomComponent/CustomEmptyComponent.tsx
@@ -0,0 +1,12 @@
+import React, { PropsWithChildren } from "react";
+import "./CustomEmptyComponent.css"
+
+const CustomEmptyComponent: React.FC<PropsWithChildren> = (props: PropsWithChildren) => {
+    return(
+        <div id="EmptyMainContent">
+            {props.children}
+        </div>
+    )
+}
+
+export default CustomEmptyComponent;
--- src/Components/EmptyMainContainerComponent/EmptyMainContainerComponent.tsx
+++ src/Components/EmptyMainContainerComponent/EmptyMainContainerComponent.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./EmptyMainContainerComponent.css"
+import CustomEmptyComponent from "../EmptyCustomComponent/CustomEmptyComponent";
 
 const EmptyMainContainerComponent: React.FC = () => {
     return(
-        <div id="EmptyMainContent">
+        <CustomEmptyComponent>
             <h1>It's empty here...</h1>
             <p>Click on a list on the left</p>
             <p>to see your tasks.</p>
-        </div>
+        </CustomEmptyComponent>
     )
 }
 
