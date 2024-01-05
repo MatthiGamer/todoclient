ccd6b8bc4d382ff7b6909640d79c7bf1d6037b83
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-01-05 09:48:00 +0100

    Added method placeholder for EmptyListComponent

--- src/Components/EmptyListComponent/EmptyListComponent.tsx
+++ src/Components/EmptyListComponent/EmptyListComponent.tsx
@@ -4,11 +4,21 @@ import ButtonComponent from "../ButtonComponent/ButtonComponent";
 import { SECONDARY_COLOR } from "../../Colors";
 import CustomEmptyComponent from "../EmptyCustomComponent/CustomEmptyComponent";
 
-const EmptyListComponent: React.FC = () => {
+interface EmptyListComponentProps {
+    method?: () => void;
+}
+
+const EmptyListComponent: React.FC<EmptyListComponentProps> = (props) => {
+
+    const HandleOnClick = () => {
+        if (!props.method) return;
+        props.method();
+    }
+
     return(
         <CustomEmptyComponent>
             <h1>It's empty here...</h1>
-            <ButtonComponent title="Click here to add a task" color={SECONDARY_COLOR} id="AddButton"/>
+            <ButtonComponent title="Click here to add a task" color={SECONDARY_COLOR} id="AddButton" OnClick={HandleOnClick}/>
             <p>or add one as usual down there ↓.</p>
         </CustomEmptyComponent>
     )
