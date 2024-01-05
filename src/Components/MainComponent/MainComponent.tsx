bef53002d6b57fcbc6588f58c03474f12e587210
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-01-05 11:34:00 +0100

    Added todo comment

--- src/Components/MainComponent/MainComponent.tsx
+++ src/Components/MainComponent/MainComponent.tsx
@@ -13,6 +13,7 @@ const MainComponent: React.FC = () => {
     <div id="MainContainer">
       <div>
         <SidebarComponent setListName={setListName}/>
+        {/* TODO: Move to its own component */}
         <CustomDialogComponent header='Where should the task be added?' isVisible={true}>
           <ButtonComponent title={'Add to Todo'} color='black'></ButtonComponent>
           <ButtonComponent title={'Add to Optional'} color='black'></ButtonComponent>
