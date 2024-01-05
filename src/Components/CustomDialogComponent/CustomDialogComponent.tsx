50f86f02b3cb5d6f784f7cea841334a4e242b9f5
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-01-05 11:01:00 +0100

    Moved cancel button into CustomDialogComponent as default content

--- src/Components/CustomDialogComponent/CustomDialogComponent.tsx
+++ src/Components/CustomDialogComponent/CustomDialogComponent.tsx
@@ -1,5 +1,6 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useState } from "react";
 import "./CustomDialogComponent.css";
+import ButtonComponent from "../ButtonComponent/ButtonComponent";
 
 interface CustomDialogComponentProps {
     header: string;
@@ -7,12 +8,16 @@ interface CustomDialogComponentProps {
 }
 
 const CustomDialogComponent: React.FC<PropsWithChildren<CustomDialogComponentProps>> = (props) => {
+    const [dialogIsVisible, setDialogVisibility] = useState<boolean>(props.isVisible);
+    const HandleOnCancel = () => setDialogVisibility(false);
+    
     return (
-        !props.isVisible ? <></> :
+        !dialogIsVisible ? <></> :
         <div id="Overlay">
             <div id="Dialog">
                 <h2>{props.header}</h2>
                 {props.children}
+                <ButtonComponent title={'Cancel'} color='black' OnClick={HandleOnCancel}></ButtonComponent>
             </div>
         </div>
     )
--- src/Components/MainComponent/MainComponent.tsx
+++ src/Components/MainComponent/MainComponent.tsx
@@ -8,17 +8,14 @@ import ButtonComponent from '../ButtonComponent/ButtonComponent';
 const MainComponent: React.FC = () => {
 
   const [listName, setListName] = useState<string>("");
-  const [dialogIsVisible, setDialogVisibility] = useState<boolean>(true);
-  const HandleOnCancel = () => setDialogVisibility(false);
 
   return (
     <div id="MainContainer">
       <div>
         <SidebarComponent setListName={setListName}/>
-        <CustomDialogComponent header='Where should the task be added?' isVisible={dialogIsVisible}>
+        <CustomDialogComponent header='Where should the task be added?' isVisible={true}>
           <ButtonComponent title={'Add to Todo'} color='black'></ButtonComponent>
           <ButtonComponent title={'Add to Optional'} color='black'></ButtonComponent>
-          <ButtonComponent title={'Cancel'} color='black' OnClick={HandleOnCancel}></ButtonComponent>
         </CustomDialogComponent>
       </div>
       <div id="ListView" style={listName === "" ? {display: "flex"} : {}}>
