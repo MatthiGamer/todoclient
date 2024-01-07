46f5e272746ccdf29444a21c80a6d08cb4cd5f5d
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-01-07 09:43:00 +0100

    Added support for pressing enter to create a new list

--- src/Components/AddListDialogComponent/AddListDialogComponent.tsx
+++ src/Components/AddListDialogComponent/AddListDialogComponent.tsx
@@ -14,16 +14,33 @@ const ADD_LIST_DIALOG_COLOR = SECONDARY_COLOR;
 
 const AddListDialogComponent: React.FC<AddListDialogComponentProps> = (props) => {
 
+    const HandleOnEnterKeyPress = (event: { key: string; }) => {
+        if (event.key !== "Enter") return;
+        HandleOnCreate();
+      };
+
     const inputRef = useRef<HTMLInputElement>(null);
     const HandleOnCreate = () => {
-        if (!inputRef.current) return;
+        if (!inputRef.current || inputRef.current.value === "") return;
         props.createNewList(inputRef.current.value);
     }
 
     return (
         !props.isVisible ? <></> :
-        <CustomDialogComponent header={"What should the new list be called?"} isVisible={true} color={ADD_LIST_DIALOG_COLOR} setDialogVisibility={props.setDialogVisibility}>
-            <input id="ListNameInput" type="text" style={{color: ADD_LIST_DIALOG_COLOR}} placeholder="Name" ref={inputRef}/>
+        <CustomDialogComponent
+        header={"What should the new list be called?"}
+        isVisible={true}
+        color={ADD_LIST_DIALOG_COLOR}
+        setDialogVisibility={props.setDialogVisibility}>
+
+            <input
+            id="ListNameInput"
+            type="text"
+            style={{color: ADD_LIST_DIALOG_COLOR}}
+            placeholder="Name"
+            onKeyDown={HandleOnEnterKeyPress}
+            ref={inputRef}/>
+
             <ButtonComponent title={"Create new List"} color={ADD_LIST_DIALOG_COLOR} OnClick={HandleOnCreate}/>
         </CustomDialogComponent>
     )
