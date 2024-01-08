86817b9f62eb3645454105026b475309549da7c9
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-01-08 16:15:29 +0100

    Removed unnecessary button and text

--- src/Components/EmptyListComponent/EmptyListComponent.tsx
+++ src/Components/EmptyListComponent/EmptyListComponent.tsx
@@ -1,25 +1,10 @@
 import React from "react";
-import "./EmptyListComponent.css"
-import ButtonComponent from "../ButtonComponent/ButtonComponent";
-import { SECONDARY_COLOR } from "../../Colors";
 import CustomEmptyComponent from "../EmptyCustomComponent/CustomEmptyComponent";
 
-interface EmptyListComponentProps {
-    method?: () => void;
-}
-
-const EmptyListComponent: React.FC<EmptyListComponentProps> = (props) => {
-
-    const HandleOnClick = () => {
-        if (!props.method) return;
-        props.method();
-    }
-
+const EmptyListComponent: React.FC = () => {
     return(
         <CustomEmptyComponent>
-            <h1>It's empty here...</h1>
-            <ButtonComponent title="Click here to add a task" color={SECONDARY_COLOR} id="AddButton" OnClick={HandleOnClick}/>
-            <p>or add one as usual down there ↓.</p>
+            <h1>All done!</h1>
         </CustomEmptyComponent>
     )
 }
