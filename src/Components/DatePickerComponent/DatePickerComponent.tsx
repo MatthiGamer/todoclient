b113d3f622b856bdbfee062b367544564e949655
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-01-08 19:50:29 +0100

    Added date picker in  DatePickerComponent

--- src/Components/DatePickerComponent/DatePickerComponent.tsx
+++ src/Components/DatePickerComponent/DatePickerComponent.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
+import "./DatePickerComponent.css";
 
 const DatePickerComponent: React.FC = () => {
+
+    const dateInputRef = useRef<HTMLInputElement>(null)
+
+    const MinDateString = (): string => {
+        const date = new Date();
+        const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
+        const month = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(date);
+        const day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
+        return `${year}-${month}-${day}`;
+    }
+
+    const today = MinDateString();
+
     return (
-        <>
-        </>
+        <div id="DatePickerContainer">
+            <input id="DateInput" type="date" lang="en" min={today} ref={dateInputRef}/>
+        </div>
     )
 }
 
