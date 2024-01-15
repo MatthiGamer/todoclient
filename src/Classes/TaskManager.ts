a99a11afc4920c2d19fa326a6640c1c5105e540b
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-01-15 21:34:07 +0100

    Removed TaskClass Added TaskListDictionary

--- src/Classes/TaskManager.ts
+++ src/Classes/TaskManager.ts
@@ -1,11 +1,13 @@
 import { SendTask } from "../Components/SignalRComponent/SignalRComponent";
-import { TaskClass } from "./TaskClass";
+import { Task } from "../Types/TaskType";
 
 export class TaskManager {
     private static instance: TaskManager | null = null;
 
     private currentList: string | null = null;
     private dueDateString: string | null = null;
+
+    private tasks: { [key: string]: Task[] } | null = null;
     
     private constructor() {}
 
@@ -27,7 +29,22 @@ export class TaskManager {
 
     public CreateTask = (taskName: string) => {
         if (!this.currentList) return;
-        const task: TaskClass = new TaskClass(taskName, this.currentList, this.dueDateString);
-        SendTask(task.GetTask());
+        const task: Task = {taskName: taskName, taskList: this.currentList, dueDateString: this.dueDateString};
+        this.AddTask(task);
+        //SendTask(task);
+    }
+
+    private AddTask = (task: Task) => {
+        if (this.tasks === null) {
+            this.tasks = {};
+        }
+
+        if (this.tasks[task.taskList] === undefined){
+            this.tasks[task.taskList] = [];
+        }
+
+        this.tasks[task.taskList] = [...this.tasks[task.taskList], task];
+        console.log("Task added.");
+        console.log(this.tasks[task.taskList]);
     }
 }
---
+++ src/Components/TaskButtonComponent/TaskButtonComponent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TaskButtonComponent: React.FC = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default TaskButtonComponent;
