38a813d852bebff61beea637125fd01edc194a7b
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-02-05 22:13:48 +0100

    Updated DateType

--- src/Classes/Utils.ts
+++ src/Classes/Utils.ts
@@ -3,7 +3,7 @@ import { DateType } from "../Types/DateType";
 export const GetDateTypeFromDate = (date: Date): DateType => {
     const dateType: DateType = {
         day: date.getDate(),
-        month: date.getMonth(),
+        month: date.getMonth() + 1, // getMonth() => January is 0 => getMonth() + 1
         year: date.getFullYear()
     };
 
