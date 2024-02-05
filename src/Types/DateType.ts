21202723b9f9dee5bf924815067ba74c1996fc9b
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-02-05 22:15:01 +0100

    Added string representation of DateType

--- src/Types/DateType.ts
+++ src/Types/DateType.ts
@@ -3,3 +3,7 @@ export type DateType = {
     month: number;
     year: number;
 }
+
+export const DateTypeString = (date: DateType | null | undefined): string => {
+    return date === null || date == undefined ? "No due date" : `${date.month}/${date.day}/${date.year}`;
+}
