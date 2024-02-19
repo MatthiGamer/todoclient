049d4b371b04ba11784ef16774bdc8c8ce960409
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2024-02-19 13:20:17 +0100

    Added client synchronisation for TaskDoneButtonComponent

--- src/Components/TaskDoneButtonComponent/TaskDoneButtonComponent.tsx
+++ src/Components/TaskDoneButtonComponent/TaskDoneButtonComponent.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import EventManager, { TASK_DONE_CHANGED_EVENT } from "../../Classes/EventManager";
 import { TaskManager } from "../../Classes/TaskManager";
 import { DONE_COLOR, SECONDARY_COLOR } from "../../Colors";
 import "../../Styles/ClickableIcons.css";
@@ -14,6 +15,20 @@ const TaskDoneButtonComponent: React.FC<TaskDoneButtonComponentProps> = (props) 
     const [isDone, setIsDone] = useState<boolean>(props.task.isDone);
     
     const TEXT_COLOR = props.color? props.color : SECONDARY_COLOR;
+
+    // Used for client synchronisation
+    useEffect(() => {
+        EventManager.addListener(TASK_DONE_CHANGED_EVENT, UpdateDone);
+        return () => {
+            EventManager.removeListener(TASK_DONE_CHANGED_EVENT, UpdateDone);
+        }
+    }, []);
+
+    // Used for client synchronisation
+    const UpdateDone = (taskID: string) => {
+        if (props.task.taskID !== taskID) return;
+        setIsDone(props.task.isDone);
+    }
 
     const HandleOnDone = () => {
         const newIsDone: boolean = !isDone;
